0eb84b47ead3f73758792c524eda5719101e035c
amarinolama <amarinolama@gmail.com>
2024-10-01 08:39:07 +0200

    feat(): añadir validador dni

--- eventos.py
+++ eventos.py
@@ -27,3 +27,30 @@ class Eventos():
         var.ui.cmbProvinciacli.clear()
         listado = conexion.Conexion.listaProv(self)
         var.ui.cmbProvinciacli.addItems(listado)
+
+    def validarDNIcli(dni):
+        try:
+            dni = str(dni).upper()
+            var.ui.txtDnicli.setText(str(dni))
+            tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
+            dig_ext = "XYZ"
+            reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = "1234567890"
+            if len(dni) == 9:
+                dig_control = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni) % 23] == dig_control:
+                    return True
+                else:
+                    var.ui.lblDnicli.setStyleSheet('background-color:#FFCC0CB;')
+                    var.ui.txtDnicli.setText(None)
+                    var.ui.txtDnicli.setFocus()
+            else:
+                var.ui.lblDnicli.setStyleSheet('background-color:#FFCC0CB;')
+                var.ui.txtDnicli.setText(None)
+                var.ui.txtDnicli.setFocus()
+
+        except Exception as error:
+            print("error en validar dni ", error)
