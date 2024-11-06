f684dcd24de6b7a0dcf40e31f15ff1f317f2a467
amarinolama <amarinolama@gmail.com>
2024-11-06 14:22:35 +0100

    feat() : añadir atajo nuevaProp

--- main.py
+++ main.py
@@ -79,6 +79,7 @@ class Main(QtWidgets.QMainWindow):
 
         var.ui.actionbarSalir.triggered.connect(eventos.Eventos.mensajeSalir)
         var.ui.actionbarLimpiar.triggered.connect(eventos.Eventos.limpiarPanel)
+        var.ui.actionTipoPropiedad.triggered.connect(eventos.Eventos.abrirTipoProp)
 
         '''
         EVENTOS DE CHECKBOX
